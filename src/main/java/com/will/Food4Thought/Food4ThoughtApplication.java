53fe42c9ce6e35fa74bfbab529a0a58ea9687d16
William Burdett <williamburdett1995@gmail.com>
2022-02-18 17:57:23 +0000

    first commit

---
+++ src/main/java/com/will/Food4Thought/Food4ThoughtApplication.java
@@ -0,0 +1,13 @@
+package com.will.Food4Thought;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Food4ThoughtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Food4ThoughtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/will/Food4Thought/Food4ThoughtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.will.Food4Thought;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Food4ThoughtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
