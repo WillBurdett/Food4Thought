55dcb543fc0bd8a63a95ada82aac1d799c83a73c
Nayan Gurung <nayangrg.se@gmail.com>
2022-02-20 21:22:52 +0000

    modify Allergies, Difficulty, and MealTime enum classes to accept lowercase enums

--- src/main/java/com/will/Food4Thought/Difficulty.java
+++ src/main/java/com/will/Food4Thought/Difficulty.java
@@ -1,7 +1,28 @@
 package com.will.Food4Thought;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Difficulty {
-    BEGINNER,
-    INTERMEDIATE,
-    ADVANCED
+    BEGINNER("BEGINNER"),
+    INTERMEDIATE("INTERMEDIATE"),
+    ADVANCED("ADVANCED");
+
+    private String difficulty;
+
+    Difficulty(String difficulty){
+        this.difficulty = difficulty;
+    }
+
+    @JsonCreator
+    public static Difficulty fromString(String difficulty) {
+        return difficulty == null
+                ? null
+                : Difficulty.valueOf(difficulty.toUpperCase());
+    }
+
+    @JsonValue
+    public String getDifficulty() {
+        return this.difficulty.toUpperCase();
+    }
 }
