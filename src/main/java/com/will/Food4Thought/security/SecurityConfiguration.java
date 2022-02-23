2e34e72334a06d3b549f1258591ea0bca2e25ff7
William Burdett <williamburdett1995@gmail.com>
2022-02-23 00:34:32 +0000

    Security working

--- src/main/java/com/will/Food4Thought/security/SecurityConfiguration.java
+++ src/main/java/com/will/Food4Thought/security/SecurityConfiguration.java
@@ -1,45 +1,33 @@
 package com.will.Food4Thought.security;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.savedrequest.NullRequestCache;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        // set config on the auth object
-        auth.inMemoryAuthentication()
-                .withUser("foo")
-                .password("foo")
-                .roles("ADMIN");
-    }
-
-    @Bean
-    public PasswordEncoder getPasswordEncoder() {
-        // NoOp is not encoding our password
-        return NoOpPasswordEncoder.getInstance();
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers( "user**").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable();
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        getHttp().authorizeHttpRequests()
-                .antMatchers("/**").hasAnyRole("ADMIN");
+        getHttp().authorizeHttpRequests().antMatchers("/").permitAll();
                 web.ignoring().antMatchers("/user");
     }
 }
+
