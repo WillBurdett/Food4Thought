551ce839c8c049b1e550028a7fee108ae3f8d856
Nayan Gurung <nayangrg.se@gmail.com>
2022-02-25 06:20:42 +0000

    allergies enum updated

--- src/main/java/com/will/Food4Thought/enums/Allergies.java
+++ src/main/java/com/will/Food4Thought/enums/Allergies.java
@@ -3,31 +3,67 @@ package com.will.Food4Thought.enums;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum Allergies {
-    DAIRY ("DAIRY"),
-    NUTS ("NUTS"),
-    SHELLFISH("SHELLFISH"),
-    SOY("SOY"),
-    WHEAT("WHEAT"),
-    SESAME("SESAME");
+    DAIRY ("DAIRY","MILK"),
+    NUTS ("NUTS","CASHEW"),
+    SHELLFISH("SHELLFISH","OYSTER"),
+    SOY("SOY","LEGUME"),
+    WHEAT("WHEAT","GLUTEN"),
+    SESAME("SESAME","SEED");
 
 
-    private String allergies;
+    private String value1;
+    private String value2;
 
-    Allergies(String allergies){
-        this.allergies = allergies;
+
+    Allergies(String value1, String value2 ){
+        this.value1 = value1;
+        this.value2=value2;
+
     }
-
 
     @JsonCreator
-    public static Allergies fromString(String phoneType) {
-        return phoneType == null
-                ? null
-                : Allergies.valueOf(phoneType.toUpperCase());
+    public static Allergies fromString(String type) {
+        for (Allergies value : Allergies.values()) {
+            if (value.value2.equals(type.toUpperCase())) {
+                return value;
+            } else if (value.value1.equals(type.toUpperCase())) {
+                return value;
+            }
+        }
+        return null;
     }
-
     @JsonValue
     public String getAllergies() {
-        return this.allergies.toUpperCase();
+        return this.value1.toUpperCase();
     }
+
+//        List<Allergies> allergiesList = new ArrayList<>();
+//        int index = 0;
+//        for (int i = 0; i < Allergies.values().length; i++) {
+//            if (Allergies.values()[i].value1.equals(type.toUpperCase())) {
+//                return Allergies.values()[i];
+//            } else if (Allergies.values()[i].value2.equals(type.toUpperCase())) {
+//                return Allergies.values()[i];
+//            }else{
+//                allergiesList.set(index++,Allergies.values()[i]);
+//                break;
+//            }
+//        }
+//        for (Allergies allergies : allergiesList) {
+//            if (allergies.value1.equals(type.toUpperCase())) {
+//                return allergies;
+//            } else if (allergies.value2.equals(type.toUpperCase())) {
+//                return allergies;
+//            }
+//        }
+//        return null;
+//    }
+//        return phoneType == null
+//                ? null
+//                : Allergies.valueOf(phoneType.toUpperCase());
+
 }
